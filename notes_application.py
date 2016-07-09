9897491e6b74660b92a09fe34e08e8d844bf2dd4
Segun Francis <francis4dblues@gmail.com>
2016-07-09 10:21:48 +0100

    add all files

--- notes_application.py
+++ notes_application.py
@@ -21,10 +21,26 @@ class NotesApplication(object):
 	def get(self, note_id):
 		if type(note_id) == int:
 			print self.notes[note_id-1]
+
+	def search(self, search_text):
+		if type(search_text) == int:
+			print "wrong input"
+		if type(search_text) == str:
+			for index,item in enumerate(self.notes):
+				if search_text in item:
+					print "Showing results for search", "'"+search_text+"'"
+					print "Note ID: ", index+1
+					print item
+					print "By ", self.author
+				elif search_text not in item:
+					print "Not found!"
+		else:
+			print "error"
+
 	
-			
 a = NotesApplication("grey")
 a.create("first note")
 a.create("second note")
 a.listNotes()
 a.get(1)
+a.search('this')
